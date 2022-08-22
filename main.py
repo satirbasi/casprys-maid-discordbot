c24a500bb99f0ce35993c406cf7dd86c1986135d
Caspry <salihbutbruwee77@gmail.com>
2022-08-22 20:07:23 +0300

    v2
    added cogs and stuff (spicy command :yum:)

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import os
+import disnake
 from dotenv import load_dotenv
 from disnake.ext import commands
 
 load_dotenv()
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
-bot = commands.Bot()
+intents = disnake.Intents.default()
+intents.members = True
+intents.presences = True
+intents.message_content = True
+
+bot = commands.Bot(intents=intents)
 
 @bot.event
 async def on_ready():
     print("Ready!!")
-    
-bot.load_extension("cogs.ping")
 
+# @bot.listen("on_message")
+# async def whatever_you_want_to_call_it(message):
+#     print(message.content)
+
+bot.load_extensions("cogs/")
 bot.run(BOT_TOKEN)
